1cc8d4880f533387ab3c90c21551fea0250b5aae
a1v0 <avc285@gmail.com>
2023-10-12 22:04:14 +0100

    Add Menu to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Menu from "./Menu";
 
 function App() {
     return (
         <div className="App">
             <Header />
+            <Menu />
             <h1>Hello!</h1>
             <p>The plan is as follows:</p>
             <ol>
