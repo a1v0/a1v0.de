1ed3561a5d59f7ac260217dcbb1eadaa529f7e29
a1v0 <avc285@gmail.com>
2023-10-12 19:30:56 +0100

    Install React

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{backgroundColor: "darkseagreen"}}>
+		<h1>Hello!</h1>
+		<p>
+			The plan is as follows:
+		</p>
+		<ol>
+			<li>Create a colour scheme</li>
+			<li>Create header bar</li>
+			<li>Create footer bar</li>
+			<li>Create an article template</li>
+			<li>Find a way to do breadcrumbs with static content</li>
+			<li>Create table of contents component</li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+			<li></li>
+		</ol>
+    </div>
+  );
+}
+
+export default App;
