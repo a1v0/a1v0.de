e72a9171fbfa6081bfd0f44f8a004d3dee89a8df
a1v0 <avc285@gmail.com>
2023-10-14 21:21:38 +0100

    Add placeholders to Menu

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -2,11 +2,22 @@ import React from "react";
 
 export default function Menu() {
     return (
-        <div>
-            <ul>
-                <li>Articles</li>
-                <li>GitHub</li>
-            </ul>
-        </div>
+        <section>
+            <div>
+                <h2>Articles</h2>
+                <ul>
+                    <li>[list of articles to go here]</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Links</h2>
+                <ul>
+                    <li>
+                        <a href="https://github.com/a1v0">GitHub</a>
+                    </li>
+                    <li>[LinkedIn?]</li>
+                </ul>
+            </div>
+        </section>
     );
 }
