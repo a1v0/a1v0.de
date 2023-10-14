27e847a777701a19a8b8d0f2343cb39ae89120a9
a1v0 <avc285@gmail.com>
2023-10-14 21:21:22 +0100

    Update plan

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,11 @@ function App() {
                 </li>
                 <li>Create header bar</li>
                 <li>Create footer bar</li>
-                <li>Create an article template</li>
+                <li>Find way to import static HTML or markdown content</li>
+                <li>
+                    Enable server-side rendering (see
+                    https://www.freecodecamp.org/news/server-side-rendering-your-react-app-in-three-simple-steps-7a82b95db82e)
+                </li>
                 <li>Find a way to do breadcrumbs with static content</li>
                 <li>Create table of contents component</li>
             </ol>
