3e54d2cad80ed7c4ce118152e56569fa93cdf797
a1v0 <avc285@gmail.com>
2023-10-15 16:04:15 +0100

    Refactor Article class to use Axios

---
+++ src/articles.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+class Article {
+    title: string;
+    date: string;
+    slug: string;
+    author: string;
+
+    constructor(
+        title: string,
+        date: string,
+        slug: string /* file name without extension */,
+        author = "a1v0"
+    ) {
+        this.title = title;
+        this.date = date;
+        this.slug = slug;
+        this.author = author;
+
+        Article.allArticles.push(this);
+    }
+
+    static allArticles: Array<Article> = [];
+
+    getContent() {
+        return axios.get("./articles/" + this.slug + ".txt");
+    }
+}
+
+const dummyArticle = new Article("Fake news!", "today", "test-article");
+
+export default Article;
