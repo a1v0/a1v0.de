36a7dc3fbffa4eb6307fab80252e494df7fac8c7
a1v0 <avc285@gmail.com>
2023-10-16 21:09:23 +0100

    Initial draft of data fetching

--- app/articles/[slug]/page.tsx
+++ app/articles/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import Article from "@/app/articles";
+import { useEffect, useState } from "react";
 
 // Return a list of `params` to populate the [slug] dynamic segment
 export function generateStaticParams() {
@@ -7,10 +8,25 @@ export function generateStaticParams() {
     });
 }
 
-export default function ArticlePage() {
+// Multiple versions of this page will be statically generated
+// using the `params` returned by `generateStaticParams`
+export default function ArticlePage({
+    params
+}: {
+    params: any /* I know this is bad practice but I couldn't find the under-the-hood data type of params */;
+}) {
+    const { slug } = params;
+    const currentArticle = Article.allArticles.find((article) => {
+        return (article.slug = slug);
+    });
+
+    currentArticle?.getContent().then(({ data }) => {
+        console.log(data);
+    });
+
     return (
         <main>
-            <h1>Article title</h1>
+            <h1>{currentArticle?.title}</h1>
         </main>
     );
 }
