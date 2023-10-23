2a5de40f097a954dd86bd903a5f53e123882a3a3
a1v0 <avc285@gmail.com>
2023-10-23 19:01:11 +0100

    Refactor Logo to LogoLink

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import styles from "./Header.module.css";
-import Logo from "./Logo";
+import LogoLink from "./LogoLink";
 
 function Header() {
     return (
         <header className={`${styles.Header} brand-colour-background`}>
             <nav className="max-width navbar ">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">
-                        <Logo />
-                        a1v0.de
-                    </a>
+                    <LogoLink />
                 </div>
             </nav>
         </header>
