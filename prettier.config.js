54d3b03986426b15dbd5ce9ea6cc02074b9b9e86
a1v0 <avc285@gmail.com>
2023-10-26 20:59:29 +0100

    Set Prettier config

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    trailingComma: "none",
+    tabWidth: 4,
+    semi: true,
+    singleQuote: false,
+    plugins: ["prettier-plugin-tailwindcss"]
+};
