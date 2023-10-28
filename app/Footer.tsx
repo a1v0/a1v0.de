c897441832aea88ca1813a7cb6519171c818a527
a1v0 <avc285@gmail.com>
2023-10-28 19:52:57 +0100

    Add text content to footer

--- app/Footer.tsx
+++ app/Footer.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Footer() {
-    return <footer></footer>;
+    return (
+        <footer className="bg-brand-colour py-2">
+            <section className="clear-gutters">
+                Copyright &copy; {new Date().getFullYear()}{" "}
+                <strong className="font-medium">a1v0.de</strong>. All rights
+                reserved.
+            </section>
+        </footer>
+    );
 }
 
 export default Footer;
