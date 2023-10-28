2d1070febc47bc627122e0b1aa2a906e2f8c7087
a1v0 <avc285@gmail.com>
2023-10-28 19:35:56 +0100

    Create column layout

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Menu() {
     return (
         <section className="bg-background-grey">
-            <div className="clear-gutters">
-                <div>
+            <div className="clear-gutters flex justify-stretch py-8">
+                <div className="flex-1">
                     <h2>Articles</h2>
                     <ul>
                         <li>[list of articles to go here]</li>
                     </ul>
                 </div>
-                <div>
+                <div className="flex-1">
                     <h2>Links</h2>
                     <ul>
                         <li>
