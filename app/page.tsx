5d58bb25f5dff85c6eae093ddd1d70ae897ff7d7
a1v0 <avc285@gmail.com>
2023-10-28 18:15:55 +0100

    Update to-do list

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,24 @@ export default function Home() {
             <h1>Hello!</h1>
             <p>The plan is as follows:</p>
             <ol style={{ marginLeft: "40px" }}>
-                <li>Replace Bootstrap with Tailwind</li>
-                <li>Create header bar</li>
+                <li>Create menu component</li>
                 <li>Create footer bar</li>
+                <li>Create home page</li>
+                <li>
+                    Create Articles page to show list of all content, so it can
+                    be spidered
+                </li>
                 <li>
                     Parse static HTML using this
                     https://medium.com/@uigalaxy7/how-to-render-html-in-react-7f3c73f5cafc
                 </li>
                 <li>Find a way to do breadcrumbs with static content</li>
-                <li>Create table of contents component</li>
+                <li style={{ textDecoration: "line-through" }}>
+                    Replace Bootstrap with Tailwind
+                </li>
+                <li style={{ textDecoration: "line-through" }}>
+                    Create header bar
+                </li>
                 <li style={{ textDecoration: "line-through" }}>
                     Enable server-side rendering (see
                     https://www.freecodecamp.org/news/server-side-rendering-your-react-app-in-three-simple-steps-7a82b95db82e)
