b20fa7a682c81fe7025ec10237206f6206b4b8ca
a1v0 <avc285@gmail.com>
2023-10-28 18:21:03 +0100

    Create clear-gutters CSS class

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,7 +5,8 @@ import LogoLink from "./LogoLink";
 import Menu from "./Menu";
 
 function Header() {
-    const [showMenu, setShowMenu] = useState(false);
+    const [showMenu, setShowMenu] = useState(true);
+    // const [showMenu, setShowMenu] = useState(false);
 
     const downChevronPath =
             "M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z",
@@ -14,7 +15,7 @@ function Header() {
     return (
         <header>
             <nav className="bg-brand-colour p-1">
-                <div className="mx-auto my-0 flex max-w-gutter items-stretch justify-between p-0">
+                <div className="clear-gutters flex items-stretch justify-between p-0">
                     <div>
                         <LogoLink />
                     </div>
--- app/Menu.tsx
+++ app/Menu.tsx
@@ -2,21 +2,25 @@ import React from "react";
 
 export default function Menu() {
     return (
-        <section>
-            <div>
-                <h2>Articles</h2>
-                <ul>
-                    <li>[list of articles to go here]</li>
-                </ul>
-            </div>
-            <div>
-                <h2>Links</h2>
-                <ul>
-                    <li>
-                        <a href="https://github.com/a1v0">GitHub</a>
-                    </li>
-                    <li>[LinkedIn?]</li>
-                </ul>
+        <section className="bg-background-grey">
+            <div className="clear-gutters">
+                <div>
+                    <h2>Articles</h2>
+                    <ul>
+                        <li>[list of articles to go here]</li>
+                    </ul>
+                </div>
+                <div>
+                    <h2>Links</h2>
+                    <ul>
+                        <li>
+                            <a href="https://github.com/a1v0">
+                                GitHub [possibly also a link to this repo?]
+                            </a>
+                        </li>
+                        <li>[LinkedIn?]</li>
+                    </ul>
+                </div>
             </div>
         </section>
     );
