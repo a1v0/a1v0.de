28275a4b927f959f7d9579877b38ef75448de76e
a1v0 <avc285@gmail.com>
2023-10-30 22:20:26 +0000

    Dynamically populate Menu with articles

--- app/Menu.tsx
+++ app/Menu.tsx
@@ -1,13 +1,27 @@
 import React from "react";
+import Article from "@/app/articles";
 
 export default function Menu() {
+    const articles = [];
+    for (let article in Article.allArticles) {
+        articles.push(Article.allArticles[article]);
+    }
+
     return (
         <section className="bg-background-grey">
             <div className="clear-gutters flex justify-stretch py-8">
                 <div className="flex-1">
                     <h2>Articles</h2>
                     <ul>
-                        <li>[list of articles to go here]</li>
+                        {articles.map((article: Article) => {
+                            return (
+                                <li>
+                                    <a href={`/articles/${article.slug}`}>
+                                        {article.title}
+                                    </a>
+                                </li>
+                            );
+                        })}
                     </ul>
                 </div>
                 <div className="flex-1">
