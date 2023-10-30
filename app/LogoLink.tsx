818842fdc17f16a14928c8ca813cc54e9246d4e0
a1v0 <avc285@gmail.com>
2023-10-30 21:53:34 +0000

    Set height property of Logo

--- app/LogoLink.tsx
+++ app/LogoLink.tsx
@@ -8,7 +8,7 @@ export default function LogoLink() {
             className="group flex flex-row items-center text-xl font-medium"
         >
             <div className="mr-2.5 inline-block rounded-bl-2xl rounded-br rounded-tl rounded-tr-2xl bg-accent-colour p-2.5 transition-all duration-500 ease-in-out group-hover:rounded-bl group-hover:rounded-br-2xl group-hover:rounded-tl-2xl group-hover:rounded-tr">
-                <Logo />
+                <Logo height="h-6" />
             </div>
             a1v0.de
         </a>
