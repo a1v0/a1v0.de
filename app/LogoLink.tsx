66b8b4d95e3909265bd64c5586d64c9035d98135
a1v0 <avc285@gmail.com>
2023-11-03 21:20:13 +0000

    Apply exceptions to global link stylings

--- app/LogoLink.tsx
+++ app/LogoLink.tsx
@@ -5,7 +5,7 @@ export default function LogoLink() {
     return (
         <a
             href="/"
-            className="group flex flex-row items-center text-xl font-medium"
+            className="group flex flex-row items-center pr-2.5 text-xl font-medium text-text-dark no-underline visited:text-text-dark visited:no-underline hover:rounded-bl hover:text-text-dark hover:no-underline active:rounded-br-2xl active:rounded-tl-2xl active:rounded-tr"
         >
             <div className="mr-2.5 inline-block rounded-bl-2xl rounded-br rounded-tl rounded-tr-2xl bg-accent-colour p-2.5 transition-all duration-500 ease-in-out group-hover:rounded-bl group-hover:rounded-br-2xl group-hover:rounded-tl-2xl group-hover:rounded-tr">
                 <Logo height="h-6" />
