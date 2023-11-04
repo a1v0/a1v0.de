bf8fbdee0c995c804cece7b98ad98c912a8a4a84
a1v0 <avc285@gmail.com>
2023-11-04 11:06:20 +0000

    Mark various tasks as MVP

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 export default function Home() {
+    const mvp = (
+        <span className="small-caps text-xs font-extrabold text-[red]">
+            MVP:
+        </span>
+    );
+
     return (
         <main>
             <div className="bg-accent-colour">
@@ -26,13 +32,13 @@ export default function Home() {
                 </p>
             </article>
             <ol className="ml-8 list-decimal">
-                <li>Create home page</li>
+                <li>{mvp} Create home page</li>
                 <li>
                     Create Articles page to show list of all content, so it can
                     be spidered
                 </li>
                 <li>
-                    Add sitemap
+                    {mvp} Add sitemap
                     https://nextjs.org/docs/app/api-reference/file-conventions/metadata/sitemap
                 </li>
                 <li>Make site mobile-friendly</li>
@@ -45,7 +51,7 @@ export default function Home() {
                     </ul>
                 </li>
                 <li>
-                    Parse static HTML using this
+                    {mvp} Parse static HTML using this
                     https://medium.com/@uigalaxy7/how-to-render-html-in-react-7f3c73f5cafc
                     <ul className="ml-8 list-disc">
                         <li>
@@ -59,22 +65,23 @@ export default function Home() {
                 <li>Attempt to use content tags</li>
                 <li>Find a way to do breadcrumbs with static content</li>
                 <li className="line-through">
-                    Replace `a` tags with `Link` tags
+                    {mvp} Replace `a` tags with `Link` tags
                 </li>
                 <li className="line-through">Add styling to menu links</li>
-                <li className="line-through">Create menu component</li>
-                <li className="line-through">Create footer bar</li>
+                <li className="line-through">{mvp} Create menu component</li>
+                <li className="line-through">{mvp} Create footer bar</li>
                 <li className="line-through">
                     Replace Bootstrap with Tailwind
                 </li>
-                <li className="line-through">Create header bar</li>
+                <li className="line-through">{mvp} Create header bar</li>
                 <li className="line-through">
+                    {mvp}
                     Enable server-side rendering (see
                     https://www.freecodecamp.org/news/server-side-rendering-your-react-app-in-three-simple-steps-7a82b95db82e)
                 </li>
-                <li className="line-through">Create a colour scheme</li>
+                <li className="line-through">{mvp} Create a colour scheme</li>
                 <li className="line-through">
-                    Find way to import static HTML or markdown content
+                    {mvp} Find way to import static HTML or markdown content
                 </li>
             </ol>
         </main>
