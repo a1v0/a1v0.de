371fa2fa0eb9de38e24e8ad96b418bea24c15f70
a1v0 <avc285@gmail.com>
2023-11-14 21:07:40 +0000

    Update text on 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -15,9 +15,9 @@ function NotFoundPage() {
                 <article className="clear-gutters">
                     <h2>What do I do now?</h2>
                     <p>
-                        I&apos;m sorry it&apos;s come to this. All that&apos;s
-                        left at this point is to go home and rethink your life.
-                        Nothing good will come of you lingering on this page.
+                        All that&apos;s left at this point is to go home and
+                        rethink your life. Nothing good will come of you
+                        lingering on this page. Quoth the Server: 404.
                     </p>
                 </article>
             </div>
