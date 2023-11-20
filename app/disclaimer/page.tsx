dcd3aac3e9b2223c3550dd7d4f5944a9f1a728d1
a1v0 <avc285@gmail.com>
2023-11-20 21:23:31 +0000

    Create boilerplate disclaimer page

---
+++ app/disclaimer/page.tsx
@@ -0,0 +1,11 @@
+export default function Disclaimer() {
+    return (
+        <main className="flex grow flex-col bg-background-white">
+            <div>
+                <article className="clear-gutters text-content bg-background-white">
+                    <h1>Disclaimer</h1>
+                </article>
+            </div>
+        </main>
+    );
+}
