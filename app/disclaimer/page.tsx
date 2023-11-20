7fff33365b35ffca5d9f2add7aec9e536d03a291
a1v0 <avc285@gmail.com>
2023-11-20 21:32:07 +0000

    Add content to disclaimer

--- app/disclaimer/page.tsx
+++ app/disclaimer/page.tsx
@@ -4,6 +4,19 @@ export default function Disclaimer() {
             <div>
                 <article className="clear-gutters text-content bg-background-white">
                     <h1>Disclaimer</h1>
+                    <p>
+                        This website, a1v0.de, can accept no liability for any
+                        incorrect or outdated information given. We do our best
+                        to ensure our posts are accurate and properly proofread,
+                        and we try to ensure that the limitations of our posts
+                        are stated clearly, but nobody's perfect.
+                    </p>
+                    <p>
+                        At the end of the day, the contents of this site are
+                        little more than opinions and thoughts of one
+                        individual. The content on this site is not
+                        peer-reviewed.
+                    </p>
                 </article>
             </div>
         </main>
