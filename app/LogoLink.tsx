25c96cc311403ddd69755c8e3c39e6564518f744
a1v0 <avc285@gmail.com>
2024-03-03 12:07:02 +0000

    Replace spaces with tabs in TSX files

--- app/LogoLink.tsx
+++ app/LogoLink.tsx
@@ -3,15 +3,15 @@ import Logo from "./Logo";
 import Link from "next/link";
 
 export default function LogoLink() {
-    return (
-        <Link
-            href="/"
-            className="group flex flex-row items-center pr-2.5 text-xl font-medium text-text-dark no-underline visited:text-text-dark visited:no-underline hover:rounded-bl hover:text-text-dark hover:no-underline active:rounded-br-2xl active:rounded-tl-2xl active:rounded-tr"
-        >
-            <div className="mr-2.5 inline-block rounded-bl-2xl rounded-br rounded-tl rounded-tr-2xl bg-accent-colour p-2.5 transition-all duration-500 ease-in-out group-hover:rounded-bl group-hover:rounded-br-2xl group-hover:rounded-tl-2xl group-hover:rounded-tr">
-                <Logo height="h-6" />
-            </div>
-            a1v0.de
-        </Link>
-    );
+	return (
+		<Link
+			href="/"
+			className="group flex flex-row items-center pr-2.5 text-xl font-medium text-text-dark no-underline visited:text-text-dark visited:no-underline hover:rounded-bl hover:text-text-dark hover:no-underline active:rounded-br-2xl active:rounded-tl-2xl active:rounded-tr"
+		>
+			<div className="mr-2.5 inline-block rounded-bl-2xl rounded-br rounded-tl rounded-tr-2xl bg-accent-colour p-2.5 transition-all duration-500 ease-in-out group-hover:rounded-bl group-hover:rounded-br-2xl group-hover:rounded-tl-2xl group-hover:rounded-tr">
+				<Logo height="h-6" />
+			</div>
+			a1v0.de
+		</Link>
+	);
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,28 +1,28 @@
 function NotFoundPage() {
-    return (
-        <main className="flex grow flex-col">
-            <div className="bg-accent-colour">
-                <header className="clear-gutters py-16">
-                    <h1 className="m-auto p-0">
-                        <span className="block">Page not found.</span>
-                        <span className="block text-softer-black">
-                            Better luck next time.
-                        </span>
-                    </h1>
-                </header>
-            </div>
-            <div className="text-content grow bg-background-white">
-                <article className="clear-gutters">
-                    <h2>What do I do now?</h2>
-                    <p>
-                        All that&apos;s left at this point is to go home and
-                        rethink your life. Nothing good will come of you
-                        lingering on this page. Quoth the Server: 404.
-                    </p>
-                </article>
-            </div>
-        </main>
-    );
+	return (
+		<main className="flex grow flex-col">
+			<div className="bg-accent-colour">
+				<header className="clear-gutters py-16">
+					<h1 className="m-auto p-0">
+						<span className="block">Page not found.</span>
+						<span className="block text-softer-black">
+							Better luck next time.
+						</span>
+					</h1>
+				</header>
+			</div>
+			<div className="text-content grow bg-background-white">
+				<article className="clear-gutters">
+					<h2>What do I do now?</h2>
+					<p>
+						All that&apos;s left at this point is to go home and
+						rethink your life. Nothing good will come of you
+						lingering on this page. Quoth the Server: 404.
+					</p>
+				</article>
+			</div>
+		</main>
+	);
 }
 
 export default NotFoundPage;
