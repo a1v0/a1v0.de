74b6d89e39d2fa3c39ac6f81a9dc74ba2ad7c48d
a1v0 <avc285@gmail.com>
2024-03-03 12:07:54 +0000

    Replace spaces with tabs in sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,54 +2,54 @@ import { MetadataRoute } from "next";
 import { allArticles } from "contentlayer/generated";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const BASE_URL = process.env.SITE_URL || "https://www.a1v0.de";
+	const BASE_URL = process.env.SITE_URL || "https://www.a1v0.de";
 
-    type changeFrequency =
-        | "always"
-        | "hourly"
-        | "daily"
-        | "weekly"
-        | "monthly"
-        | "yearly"
-        | "never";
+	type changeFrequency =
+		| "always"
+		| "hourly"
+		| "daily"
+		| "weekly"
+		| "monthly"
+		| "yearly"
+		| "never";
 
-    interface SitemapEntry {
-        url: string;
-        lastModified: string;
-        changeFrequency: changeFrequency;
-    }
+	interface SitemapEntry {
+		url: string;
+		lastModified: string;
+		changeFrequency: changeFrequency;
+	}
 
-    const yearlyChangeFrequency = "yearly" as changeFrequency;
+	const yearlyChangeFrequency = "yearly" as changeFrequency;
 
-    const allPages = allArticles.map((article) => {
-        const url = BASE_URL + article.url,
-            lastModified = article.date,
-            changeFrequency = yearlyChangeFrequency;
+	const allPages = allArticles.map((article) => {
+		const url = BASE_URL + article.url,
+			lastModified = article.date,
+			changeFrequency = yearlyChangeFrequency;
 
-        const sitemapEntry: SitemapEntry = {
-            url,
-            lastModified,
-            changeFrequency
-        };
+		const sitemapEntry: SitemapEntry = {
+			url,
+			lastModified,
+			changeFrequency
+		};
 
-        return sitemapEntry;
-    });
+		return sitemapEntry;
+	});
 
-    const routes = ["", "/articles"]; // add any other routes here
+	const routes = ["", "/articles"]; // add any other routes here
 
-    routes.forEach((route) => {
-        const url = BASE_URL + route,
-            lastModified = new Date().toISOString(),
-            changeFrequency = yearlyChangeFrequency;
+	routes.forEach((route) => {
+		const url = BASE_URL + route,
+			lastModified = new Date().toISOString(),
+			changeFrequency = yearlyChangeFrequency;
 
-        const sitemapEntry: SitemapEntry = {
-            url,
-            lastModified,
-            changeFrequency
-        };
+		const sitemapEntry: SitemapEntry = {
+			url,
+			lastModified,
+			changeFrequency
+		};
 
-        allPages.push(sitemapEntry);
-    });
+		allPages.push(sitemapEntry);
+	});
 
-    return allPages;
+	return allPages;
 }
