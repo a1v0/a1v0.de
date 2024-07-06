c70605d8c7f557aaa37bd6e00e7991991cf486bb
a1v0 <avc285@gmail.com>
2024-07-06 13:51:19 +0100

    Update to-dos

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -3,7 +3,11 @@
 // - create a page for all categories
 // - sub-page each category, populated by slug
 // - move the articles page into a slug subfolder so that each article renders as part of its category
-//
+// - if possible, find a way to map categories to names, so that:
+//   a) all categories can be lowercase in the articles
+//   b) while a category may be named C-Sharp in the backend, the frontend can display C# (C-Sharp looks rubbish)
+//   - this could be achieved potentially by creating a simple dictionary that maps categories to their display
+//     names (any any other data I might want to pass in)
 //
 // https://medium.com/frontendweb/how-to-build-the-static-blog-with-content-layer-markdown-and-nextjs-29f6ac93d06
 // this article can guide if necessary
