9770afbe116d07350b9b8818142210519d272e07
a1v0 <avc285@gmail.com>
2024-07-06 18:08:41 +0100

    Create mapping table for category names

---
+++ app/article-categories.tsx
@@ -0,0 +1,20 @@
+export const categories = {
+	"c-sharp": {
+		name: "C#"
+	},
+	general: {
+		name: "General"
+	},
+	"next.js": {
+		name: "Next.js"
+	},
+	regex: {
+		name: "Regular expressions (regex)"
+	},
+	servicenow: {
+		name: "ServiceNow"
+	},
+	"ups-api": {
+		name: "UPS API"
+	}
+};
