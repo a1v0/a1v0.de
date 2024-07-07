8f68f509b549c181286d75118a94b8f9483d8d74
a1v0 <avc285@gmail.com>
2024-07-07 10:21:06 +0100

    Move categories into main directory

---
+++ app/[category]/page.tsx
@@ -0,0 +1,101 @@
+//
+// to-do:
+// - create a page for all categories
+// - sub-page each category, populated by category
+// - move the articles page into a category subfolder so that each article renders as part of its category
+//
+// https://medium.com/frontendweb/how-to-build-the-static-blog-with-content-layer-markdown-and-nextjs-29f6ac93d06
+// this article can guide if necessary
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+import { categoriesMap } from "@/app/article-categories";
+import { allArticles } from "contentlayer/generated";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+export const generateStaticParams = async () => {
+	return allArticles.map((article) => {
+		return { category: article.category.toLowerCase() };
+	});
+};
+
+export const generateMetadata = ({
+	params
+}: {
+	params: { category: string };
+}) => {
+	const post = allArticles.find((post) => {
+		return post.category.toLowerCase() === params.category.toLowerCase();
+	});
+	if (!post) {
+		return notFound();
+	}
+
+	const categoryName = categoriesMap[post.category].displayName;
+	return { title: categoryName };
+};
+
+const PostLayout = ({ params }: { params: { category: string } }) => {
+	const post = allArticles.find(
+		(post) => post.category.toLowerCase() === params.category.toLowerCase()
+	);
+	if (!post) {
+		return notFound();
+	}
+
+	const categoryName = categoriesMap[post.category].displayName;
+
+	return (
+		<main className="grow bg-background-white">
+			<div>
+				<article className="clear-gutters text-content bg-background-white">
+					<div>
+						<h1>{categoryName}</h1>
+					</div>
+					<div>
+						Articles in this category:
+						<ul>
+							{allArticles.map((article, index) => {
+								const articleIsInCategory =
+									article.category === post.category;
+								return articleIsInCategory ? (
+									<li key={index}>
+										<Link href={article.url}>
+											{article.title}
+										</Link>
+									</li>
+								) : null;
+							})}
+						</ul>
+					</div>
+				</article>
+			</div>
+		</main>
+	);
+};
+
+export default PostLayout;
