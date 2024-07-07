0d01a69064bd1ea759ac7afa5aac8988b924baca
a1v0 <avc285@gmail.com>
2024-07-07 10:48:45 +0100

    Add categories to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,6 @@
 import { MetadataRoute } from "next";
 import { allArticles } from "contentlayer/generated";
+import { categoriesMap } from "./article-categories";
 
 export default function sitemap(): MetadataRoute.Sitemap {
 	const BASE_URL = process.env.SITE_URL || "https://www.a1v0.de";
@@ -23,7 +24,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
 
 	const allPages: SitemapEntry[] = [];
 
-	const staticRoutes = ["", "/disclaimer"]; // add any other static routes here
+	const staticRoutes = ["/", "/disclaimer"]; // add any other static routes here
 
 	staticRoutes.forEach((route) => {
 		const url = BASE_URL + route,
@@ -42,6 +43,20 @@ export default function sitemap(): MetadataRoute.Sitemap {
 	allArticles.forEach((article) => {
 		const url = BASE_URL + article.url,
 			lastModified = article.date,
+			changeFrequency = yearlyChangeFrequency;
+
+		const sitemapEntry: SitemapEntry = {
+			url,
+			lastModified,
+			changeFrequency
+		};
+
+		allPages.push(sitemapEntry);
+	});
+
+	Object.keys(categoriesMap).forEach((category) => {
+		const url = BASE_URL + "/" + category,
+			lastModified = new Date().toISOString(),
 			changeFrequency = yearlyChangeFrequency;
 
 		const sitemapEntry: SitemapEntry = {
