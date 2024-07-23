d204ccb7ad28ea7dcd62b87c708e11dc1461b09f
a1v0 <avc285@gmail.com>
2024-07-23 20:51:09 +0100

    Create pre-build linter file

---
+++ pre-build-checks.ts
@@ -0,0 +1,30 @@
+const reset = "\x1b[0m";
+
+const fgBlack = "\x1b[30m",
+	fgRed = "\x1b[31m",
+	fgGreen = "\x1b[32m",
+	fgYellow = "\x1b[33m",
+	fgBlue = "\x1b[34m",
+	fgMagenta = "\x1b[35m",
+	fgCyan = "\x1b[36m",
+	fgWhite = "\x1b[37m",
+	fgGray = "\x1b[90m";
+
+const indentation = "   ";
+
+console.log(
+	`${indentation}${fgRed}================================================${reset}`
+);
+console.log(
+	`${indentation}${fgRed}| ERROR: category missing from \`categoriesMap\` |${reset}`
+);
+console.log(
+	`${indentation}${fgRed}================================================${reset}`
+);
+console.log();
+console.log(
+	`${indentation}The following categories are missing from the \`categoriesMap\` dictionary:`
+);
+
+console.log();
+console.log();
