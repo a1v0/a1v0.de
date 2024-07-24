c5ab9e778e51825514104ae215707897ee3e1ddb
a1v0 <avc285@gmail.com>
2024-07-24 21:15:03 +0100

    Remove obsolete comment

--- utils/getPostMetadata.ts
+++ utils/getPostMetadata.ts
@@ -10,44 +10,6 @@ export interface PostMetadata {
 }
 
 export default function getPostMetadata(category: string) {
-	// change this so that, under certain conditions, it returns EVERYTHING, irrespective of category
-	//
-	// this would be useful for the sitemap, as well as the generateStaticParams method in the article page
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
-	//
 	const folder = `articles/${category}/`;
 	const allFiles = fs.readdirSync(folder);
 	const markdownFiles = allFiles.filter((file) => {
