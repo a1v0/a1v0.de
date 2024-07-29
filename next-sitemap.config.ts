b930c513a59d0a3d2036b569b0753c6c94986af6
a1v0 <avc285@gmail.com>
2024-07-29 20:21:05 +0100

    Change file to TypeScript file

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,5 +1,6 @@
 /** @type {import("next-sitemap").IConfig} */
 module.exports = {
 	siteUrl: process.env.SITE_URL || "https://www.a1v0.de",
-	generateRobotsTxt: true
+	generateRobotsTxt: true,
+	changefreq: "yearly"
 };
