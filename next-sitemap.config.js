c6e5a2fdf07ec4cb1971593db5b7368c3478ef82
a1v0 <avc285@gmail.com>
2024-07-29 20:12:03 +0100

    Add `postbuild` script

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next-sitemap').IConfig} */
+/** @type {import("next-sitemap").IConfig} */
 module.exports = {
 	siteUrl: process.env.SITE_URL || "https://www.a1v0.de",
 	generateRobotsTxt: true
