007eb30136e8e198d5b59036a85559c098228772
a1v0 <avc285@gmail.com>
2024-08-10 21:58:41 +0100

    Create placeholder Contact page

---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+	title: "Contact"
+};
+
+export default function Disclaimer() {
+	return (
+		<main className="flex grow flex-col bg-background-white">
+			<div>
+				<article className="clear-gutters text-content bg-background-white">
+					<h1>Contact a1v0</h1>
+					<p>
+            CONTENT GOES HERE!
+					</p>
+				</article>
+			</div>
+		</main>
+	);
+}
