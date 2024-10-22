652bd79f6a6e259d5a0e518b3a74f9a7d1d905e1
a1v0 <avc285@gmail.com>
2024-10-22 21:38:25 +0100

    Add aria label to header

--- components/Header.tsx
+++ components/Header.tsx
@@ -20,7 +20,7 @@ function Header() {
 			"M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z";
 	return (
 		<header>
-			<nav className="bg-brand-colour">
+			<nav className="bg-brand-colour" aria-label="Main">
 				<div className="clear-gutters flex items-stretch justify-between p-0">
 					<div className="py-1">
 						<LogoLink />
