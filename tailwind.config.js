60fb3422a67ad11897c60c40c672d9db5ec79ff1
a1v0 <avc285@gmail.com>
2025-01-11 21:58:41 +0000

    Add light grey colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,6 +21,7 @@ module.exports = {
 			// Dark-mode colours
 			"dark-mode-background-grey": "#2c2a29",
 			"dark-mode-background-dark-grey": "#121212",
+			"dark-mode-background-light-grey": "#404040",
 			"dark-mode-text-light": "#ccc9c9",
 			"dark-mode-accent-colour": "#f0c346", // I'm not perfectly happy with this shade, but it seems good enough
 			"dark-mode-link-normal": "#7ea2c2",
