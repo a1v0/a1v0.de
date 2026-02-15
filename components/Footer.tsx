7a9e1d24113b37b9e7c50d571ea48bb680a43870
a1v0 <avc285@gmail.com>
2026-02-15 18:48:50 +0000

    Remove "all rights reserved" notice

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,13 +4,12 @@ import Link from "next/link";
 
 function Footer() {
 	return (
-		<footer className="bg-brand-colour py-2 dark:bg-dark-mode-background-dark-grey dark:text-dark-mode-text-light [&_a]:text-text-dark dark:[&_a]:text-dark-mode-text-light dark:[&_a]:active:text-text-dark">
+		<footer className="bg-brand-colour dark:bg-dark-mode-background-dark-grey dark:text-dark-mode-text-light [&_a]:text-text-dark dark:[&_a]:text-dark-mode-text-light dark:[&_a]:active:text-text-dark py-2">
 			<div className="clear-gutters flex items-center justify-between">
 				<section className="flex grow-3 flex-col">
 					<p className="m-0">
 						Copyright &copy; {new Date().getFullYear()}{" "}
-						<strong className="font-medium">a1v0.de</strong>. All
-						rights reserved.
+						<strong className="font-medium">a1v0.de</strong>.
 					</p>
 					<nav aria-label="Footer">
 						<ul className="flush m-0 flex flex-wrap pt-2 [&>li]:m-0 [&>li]:pr-5">
